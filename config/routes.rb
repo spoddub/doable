b92bd9c631cfabefe71942762b6661312377a364
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-10-01 16:34:10 +0300

    add deletion

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   post '/projects', to: 'projects#create'
   patch '/projects/:id', to: 'projects#update'
+  delete '/projects/:id', to: 'projects#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
